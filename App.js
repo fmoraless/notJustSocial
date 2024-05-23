6e73ab59090041db55fe1eac70ac390e5257470e
fmoraless <franticzero@gmail.com>
2024-05-22 20:21:33 -0400

    footer buttons section

--- App.js
+++ App.js
@@ -1,10 +1,94 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import LikeImage from "./assets/images/like.png";
+import {
+  MaterialCommunityIcons,
+  Entypo,
+  FontAwesome5,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
+
+const post = {
+  id: "p1",
+  createdAt: "19 m",
+  User: {
+    id: "u1",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
+    name: "Francisco Morales",
+  },
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+  numberOfLikes: 11,
+  numberOfShares: 2,
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <View style={styles.post}>
+        {/* Header */}
+        <View style={styles.header}>
+          <Image
+            source={{ uri: post.User.image }}
+            style={styles.profileImage}
+          />
+          <View>
+            <Text style={styles.name}>{post.User.name}</Text>
+            <Text style={styles.subtitle}>9 m</Text>
+          </View>
+
+          <Entypo
+            name="dots-three-horizontal"
+            size={24}
+            color="gray"
+            style={styles.icon}
+          />
+        </View>
+        {/* Body */}
+        <Text style={styles.description}>{post.description}</Text>
+        {post.image && (
+          <Image
+            source={{ uri: post.image }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        )}
+        {/* Footer */}
+        <View style={styles.footer}>
+          <View style={styles.statsRow}>
+            <Image source={LikeImage} style={styles.likeIcon} />
+            <Text style={styles.likedBy}>
+              Elon Musk and {post.numberOfLikes} likes them
+            </Text>
+            <Text style={styles.shares}>{post.numberOfShares} shares</Text>
+          </View>
+
+          {/* Buttons row */}
+          <View style={styles.buttonsRow}>
+            <View style={styles.iconButton}>
+              <SimpleLineIcons name="like" size={20} color="gray" />
+              <Text style={styles.iconButtonText}>Like</Text>
+            </View>
+
+            <View style={styles.iconButton}>
+              <FontAwesome5 name="comment-alt" size={18} color="gray" />
+              <Text style={styles.iconButtonText}>Comment</Text>
+            </View>
+
+            <View style={styles.iconButton}>
+              <MaterialCommunityIcons
+                name="share-outline"
+                size={24}
+                color="gray"
+              />
+              <Text style={styles.iconButtonText}>Share</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +98,82 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    justifyContent: "flex-start",
     alignItems: "center",
-    justifyContent: "center",
+    marginTop: 20,
+  },
+
+  post: {
+    width: "100%",
+    marginVertical: 10,
+    backgroundColor: "#fff",
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: "500",
+  },
+  subtitle: {
+    color: "gray",
+  },
+  icon: {
+    marginLeft: "auto",
+  },
+  // body
+  description: {
+    paddingHorizontal: 10,
+    lineHeight: 20,
+    letterSpacing: 0.3,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    marginTop: 10,
+  },
+  footer: {
+    paddingHorizontal: 10,
+  },
+  statsRow: {
+    paddingVertical: 10,
+    flexDirection: "row",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    paddingVertical: 10,
+    borderColor: "lightgray",
+  },
+  likeIcon: {
+    width: 20,
+    height: 20,
+    marginRight: 5,
+  },
+  likedBy: {
+    color: "gray",
+  },
+  shares: {
+    marginLeft: "auto",
+    color: "gray",
+  },
+  buttonsRow: {
+    marginVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  iconButton: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  iconButtonText: {
+    marginLeft: 5,
+    color: "gray",
+    fontWeight: "500",
   },
 });
